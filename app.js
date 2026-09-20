3b747ce255226b4936c3d09c3e5025eec97a3038
Sinergia en Acción <fabinna78@gmail.com>
2026-09-20 14:00:11 -0300

    Agregar interacciones iniciales Julilen

---
+++ app.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener("click",e=>{const target=document.querySelector(a.getAttribute("href"));if(target){e.preventDefault();target.scrollIntoView({behavior:"smooth",block:"start"})}}));});
